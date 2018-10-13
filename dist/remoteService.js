be80638578f27d69be7deddc4d60293ddcaf6f35
Claudio De Angelis <claudio.deangelis@cloud-iq.com>
2018-10-14 09:31:58 +1100

    deploy #4

--- dist/remoteService.js
+++ dist/remoteService.js
@@ -2,12 +2,10 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+// const { errors } = require('celebrate');
 
-var _require = require('celebrate'),
-    errors = _require.errors;
-
-var _require2 = require('./utils/mongoUtils'),
-    connectToDB = _require2.connectToDB;
+var _require = require('./utils/mongoUtils'),
+    connectToDB = _require.connectToDB;
 
 require('dotenv').config();
 
@@ -27,7 +25,7 @@ app_remote.use(bodyParser.urlencoded({ extended: true }));
 
 require('./routes')(app_remote);
 
-app_remote.use(errors());
+// app_remote.use(errors());
 app_remote.listen(port);
 
 console.log('#-- Server Listening on ' + process.env.REMOTE_HOST + ' port: ' + port);
--- src/remoteService.js
+++ src/remoteService.js
@@ -2,7 +2,7 @@
 
 const express =require('express');
 const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
+// const { errors } = require('celebrate');
 
 const { connectToDB }= require('./utils/mongoUtils');
 
@@ -23,7 +23,7 @@ app_remote.use(bodyParser.urlencoded({extended:true}));
 
 require('./routes')(app_remote);
 
-app_remote.use(errors());
+// app_remote.use(errors());
 app_remote.listen(port);
 
 console.log(`#-- Server Listening on ${process.env.REMOTE_HOST} port: ${port}`);
